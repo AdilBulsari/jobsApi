d20cee70dbcc22abb8ebeb5f93fbe173dbbbfcf1
AdilBulsari <adilbulsari@gmail.com>
2022-09-16 15:36:51 +0100

    added user register control

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,14 @@
+const { StatusCodes } = require("http-status-codes");
+const UserModelSchema = require("../models/User.model");
+
 exports.register = async (req, res) => {
-  res.send("Register user");
+  const user = await UserModelSchema.create({
+    ...req.body,
+  });
+
+  console.log(user);
+
+  res.status(StatusCodes.CREATED).json(user);
 };
 
 exports.login = async (req, res) => {
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { register, login } = require("../controllers/auth.controller");
 const router = express.Router();
 
-router.post(login);
-router.post(register);
+router.post("/login", login);
+router.post("/register", register);
 
 module.exports = router;
